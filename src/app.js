e672380965f8c93471655192fc49cd97348aca5f
emcog <robin.leonard@gmail.com>
2021-01-11 21:59:55 +0000

    basic app working, done button broken

--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@ import Vue from 'vue';
 
 document.addEventListener("DOMContentLoaded", () => {
     new Vue({
-        el: "app",
+        el: "#app",
         data: {
             tasks: [
-                {name: "Make it simple", isDone: false },
-                {name: "Keep on task", isDone: false }
+                { name: "Make it simple", isDone: false },
+                { name: "Keep on task", isDone: false }
             ],
         newTask:""
         },
         methods: {
             saveNewTask: function() {
-                this.task.push({name: this.newTask, isDone: false });
+                this.tasks.push({name: this.newTask, isDone: false });
                 this.newTask = "";
             },
             taskDone: function(index){
